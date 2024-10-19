48b829f64a9b6a24477aab73314e98421efeba9d
Amund Halgunset <amundhalgunset@gmail.com>
2024-10-20 00:57:57 +0200

    jest and babel added and installed, no tests made yet

---
+++ jest.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Add browser globals like `document`, `fetch`, etc.
+}, },
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.test.js"], // Applies only to Jest test files
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest, // Add Jest globals like `describe`, `it`, etc.
+      },
+}, rules: {
+      ...pluginJest.configs.recommended.rules,
+    },
+}, ];
